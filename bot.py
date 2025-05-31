bda195742232993c7a101b158ada38b9d2ebf8f3
SweetHarumi <zoyaloverr1@gmail.com>
2025-05-31 12:00:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Estou vivo!")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot rodando...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
 
